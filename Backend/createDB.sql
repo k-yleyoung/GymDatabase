f8ea2a1cc4f131d5cad69ef9111255888a7283ee
Kyle <youngkyle731@gmail.com>
2024-04-28 21:15:03 -0500

    Fixed SQL Commands

--- Backend/createDB.sql
+++ Backend/createDB.sql
@@ -49,7 +49,8 @@ INSERT INTO Class (class_name, duration, capacity, instructor_name, enrollment_c
 ('Zumba', 50, 30, 'Carol White', 25),
 ('CrossFit', 70, 15, 'Dave Brown', 10),
 ('Kickboxing', 60, 20, 'Eve Black', 15),
-('Meditation', 30, 15, 'Frank Gray', 10);
+('Meditation', 30, 15, 'Frank Gray', 10),
+('no_class', 0, 1000, 'None', 0);
 
 
 INSERT INTO Gym (name, street, city, STATE, zipcode) VALUES
@@ -61,5 +62,10 @@ INSERT INTO Employee (Name) VALUES
 ('Alice Johnson'),
 ('Bob Brown'),
 ('Eve White');
+
+INSERT INTO Member (name, dob, age, street, city, state, zipcode, phone, monthly_fee, start_date, class_name) VALUES
+('Alex Murphy', '1995-07-21', 28, '321 River Rd', 'Metropolis', 'NY', 10002, '5551234567', 30.00, '2024-04-28', 'Yoga'),
+('Sarah Connor', '1980-09-19', 43, '587 Elm St', 'Metropolis', 'NY', 10003, '5552345678', 35.00, '2024-04-28', 'Pilates'),
+('Rick Grimes', '1970-03-15', 53, '999 Maple Ave', 'Metropolis', 'NY', 10004, '5553456789', 40.00, '2024-04-28', 'no_class');
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
